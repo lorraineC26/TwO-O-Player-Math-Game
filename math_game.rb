0e2dd4c2b0b5a1ef92a46ac6be80334009333083
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-14 22:59:23 -0500

    refactor question

--- math_game.rb
+++ math_game.rb
@@ -2,5 +2,33 @@ require_relative 'player'
 require_relative 'math_question'
 
 class MathGame
+  attr_reader :players, :current_player_index
+
+  def initialize(players)
+    @players = players
+    @current_player_index = 0
+  end
+
+  # call the fx from math_question.rb
+  def generate_question()
+    # create a new instance of MathQuestion with random num
+    MathQuestion.generate_question
+  end
+
+  def current_question
+    # when @current_question is truthy -> stay unchanged
+    # otherwise, assigns the result of generate_question to @current_question
+    # returns an instance of MathQuestion
+    @current_question ||= generate_question
+  end
+
+  def validate_answer(answer)
+    # current_question method returns an instance of MathQuestion
+    # check_answer is a public method of it
+    current_question.check_answer(answer)
+  end
+
   
+
+
 end
--- math_question.rb
+++ math_question.rb
@@ -1,18 +1,18 @@
 class MathQuestion
   attr_reader :num1, :num2, :answer
 
-  def initialize
-    generate_question
-  end
-
-  def generate_question
-    # pick a random integer from 1 to 20(inclusive)
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
+  def initialize(num1, num2)
+    @num1 = num1
+    @num2 = num2
     @answer = @num1 + @num2
   end
 
-  def check_ans(player_ans)
+  def self.generate_question
+    # generate a new MathQuestion obj with random integer picked from 1 to 20 inclusively
+    new(rand(1..20), rand(1..20))
+  end
+
+  def check_answer(player_ans)
     player_ans.to_i == @answer
   end
 
